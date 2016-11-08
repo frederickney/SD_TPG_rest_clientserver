0377b62d07f698a30383eddad8b0f0f2747fc5fd
frederick ney <frederick.ney@gmail.com>
2016-11-08 15:45:16 +0100

    updating request method to the tpg server and the database

---
+++ TPG_PROJECT/packages/server_rest/client_tpg/client.py
@@ -0,0 +1,87 @@
+__author__ = 'Frederick NEY & Stephane Overlen'
+
+from .sources import *
+import pymysql as mysql
+import requests
+import xml.etree.ElementTree as xmlparser
+
+
+def mysql_connection():
+    return mysql.connect(MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_DB)
+
+
+def mysql_query(query):
+    cnx = mysql_connection()
+    cursor = cnx.cursor()
+    result = cursor.execute(query)
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+    return result
+
+
+"""
+@:param datatype string
+"""
+
+
+# request example
+# http://prod.ivtr-od.tpg.ch/v1/GetDisruptions?key=hZELIENF7EHRoHL2rY7i
+def get_distribution(datatype):
+    url = "http://" + TPG_SERVER + "/" + SERVER_VERSION + "/GetDisruptions"
+    if None == datatype:
+        url = url + "." + datatype
+    url = url + "?key=" + AUTH_KEY
+    return requests.get(url)
+
+
+# request example
+# ALL
+# http://prod.ivtr-od.tpg.ch/v1/GetStops?key=hZELIENF7EHRoHL2rY7i
+# using stop code
+# http://prod.ivtr-od.tpg.ch/v1/GetStops?key=hZELIENF7EHRoHL2rY7i&stopCode=ACCM
+# using stop name
+# http://prod.ivtr-od.tpg.ch/v1/GetStops?key=hZELIENF7EHRoHL2rY7i&stopName=gare cornavin
+# using latitude and longitude
+# http://prod.ivtr-od.tpg.ch/v1/GetStops?key=hZELIENF7EHRoHL2rY7i&GetStops?latitude=46.218176&longitude=6.146445
+# using line code
+# http://prod.ivtr-od.tpg.ch/v1/GetStops?key=hZELIENF7EHRoHL2rY7i&GetStops?lineCode=12
+def get_stops(datatype, request_code, request_data):
+    url = "http://" + TPG_SERVER + "/" + SERVER_VERSION + "/GetDisruptions"
+    param = {'key': AUTH_KEY}
+    if not None == datatype:
+        url = url + "." + datatype
+    return requests.get(url, param)
+
+
+# using stop code
+# http://prod.ivtr-od.tpg.ch/v1/GetPhysicalStops?key=hZELIENF7EHRoHL2rY7i&stopCode=ACCM
+# using stop name
+# http://prod.ivtr-od.tpg.ch/v1/GetPhysicalStops?key=hZELIENF7EHRoHL2rY7i&stopName=gare cornavin
+def get_physical_stops(datatype, request_code, request_data):
+    url = "http://" + TPG_SERVER + "/" + SERVER_VERSION + "/GetDisruptions"
+    if not None == datatype:
+        url = url + "." + datatype
+    for data in request_data:
+        param = {'key': AUTH_KEY, request_code: data}
+        result = requests.get(url, param)
+    return
+
+
+# request example
+# using departure code
+# http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures?key=hZELIENF7EHRoHL2rY7i&departureCode=22936
+# using line code
+# http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures?key=hZELIENF7EHRoHL2rY7i&lineCode=12
+# using destination code
+# http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures?key=hZELIENF7EHRoHL2rY7i&destinationCode=BOURG D'EN HAUT
+# using stop code
+# http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures?key=hZELIENF7EHRoHL2rY7i&stopCode=ACCM
+def get_next_departure(datatype, request_code, request_data):
+    url = "http://" + TPG_SERVER + "/" + SERVER_VERSION + "/GetDisruptions"
+    if not None == datatype:
+        url = url + "." + datatype
+    for data in request_data:
+        param = {'key': AUTH_KEY, request_code: data}
+        result = requests.get(url, param)
+    return
---
+++ TPG_PROJECT/packages/server_rest/client_tpg/sources.py
@@ -0,0 +1,15 @@
+__author__ = 'Frederick NEY & Stephane Overlen'
+import sys
+import struct
+import os
+
+
+AUTH_KEY = "hZELIENF7EHRoHL2rY7i"
+STOP_TIME = "3h30"
+RUNNING_TIME = "4h15"
+MYSQL_USER = "root"
+MYSQL_PASS = ""
+MYSQL_HOST = ""
+MYSQL_DB = "TPG"
+TPG_SERVER = "prod.ivtr-od.tpg.ch/"
+SERVER_VERSION = "v1"
